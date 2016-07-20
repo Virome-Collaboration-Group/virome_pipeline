8fc90815c2a1aa73f22c00284b6eb249584d4a08
Dan Nasko <dan.nasko@gmail.com>
2016-07-20 14:05:21 -0400

    Removed the local Ergatis SVN stuff. Now Shaun can install/config latest Ergatis and we can use that. Need to make sure the autopipe_package scripts play nice with the latest Ergatis version.

--- autopipe_package/ergatis/lib/Ergatis/IdGenerator/Config.pm
+++
@@ -1,9 +0,0 @@
-package Ergatis::IdGenerator::Config;
-
-## This config file defines the module that will be used for id generation by ergatis
-## The specified class should at least implement all methods defined in Ergatis::IdGenerator
-
-use Ergatis::IdGenerator::RandomIdGenerator;
-our $class = 'Ergatis::IdGenerator::RandomIdGenerator';
-
-1;
