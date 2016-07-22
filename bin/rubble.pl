ef818628855d5e5a669159a81db855cea19390ec
Dan Nasko <dan.nasko@gmail.com>
2016-07-22 15:54:21 -0400

    More rubble bug fixes

--- bin/rubble.pl
+++ bin/rubble.pl
@@ -75,6 +75,10 @@ Displays the usage message.  (Optional)
 
 Displays full manual.  (Optional) 
 
+=item B<-b, --debug>
+
+Debug mode. Will supress the removal of the working directory. (Optional)
+
 =back
 
 =head1 DEPENDENCIES
@@ -163,6 +167,7 @@ print `mkdir -p $working_dir`;
 print `mkdir -p $working_dir/0-blast_clust`;
 print `mkdir -p $working_dir/1-cull`;
 print `mkdir -p $working_dir/2-restrict`;
+print `mkdir -p $working_dir/3-blast_final`;
 
 #################################################
 ## 1. Initial BLAST against clustered BLAST DB ##
@@ -232,12 +237,13 @@ close(OUT);
 ## 4. Final BLAST using the restriction list ##
 ###############################################
 if ($threads == 1) {
-    my $blast_exe = "blastp -query " . "$working_dir/1-cull/query_cull.fasta" . " -db " . $db . " -out " . $out . " -evalue " . $evalue . " -outfmt \"6 std ppos\"" . " -seqidlist " . "$working_dir/2-restrict/restrict.txt" . " -dbsize " . $residues . " -max_target_seqs " . $max_target_seqs;
+    my $blast_exe = "blastp -query " . "$working_dir/1-cull/query_cull.fasta" . " -db " . $db . " -out " . $out . " -evalue " . $evalue . " -outfmt \"6 std ppos score salltitles\" " . " -seqidlist " . "$working_dir/2-restrict/restrict.txt" . " -dbsize " . $residues . " -max_target_seqs " . $max_target_seqs;
     print `$blast_exe`;
 }
 else {
     my $passthrough = " -seqidlist " . "$working_dir/2-restrict/restrict.txt" . " -dbsize " . $residues;
-    para_blastp($query, $db, "$working_dir/0-blast_clust/", $evalue, $threads, $max_target_seqs, $passthrough);
+    para_blastp("$working_dir/1-cull/query_cull.fasta", $db, "$working_dir/3-blast_final/", $evalue, $threads, $max_target_seqs, $passthrough);
+    print `mv $working_dir/3-blast_final/out.btab $out`;
 }
 
 
@@ -262,7 +268,7 @@ sub para_blastp
     my $seqs_per_thread = seqs_per_thread($seqs, $threads);
     my $nfiles = split_multifasta($q, "$o/para_blastp", "split", $seqs_per_thread, $t);
     for (my $i=1; $i<=$nfiles; $i++) {
-	my $blast_exe = "blastp -query $o/para_blastp/split-$i.fsa -db $dbClust -out $o/para_blastp/$i.btab -outfmt \"6 std ppos score salltitles\" -evalue $evalue -max_target_seqs $max " . $pass;
+	my $blast_exe = "blastp -query $o/para_blastp/split-$i.fsa -db $d -out $o/para_blastp/$i.btab -outfmt \"6 std ppos score salltitles\" -evalue $evalue -max_target_seqs $max " . $pass;
 	push (@THREADS, threads->create('task',"$blast_exe"));
     }
     foreach my $thread (@THREADS) {
